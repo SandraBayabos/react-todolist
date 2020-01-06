01e50fb4e6c480a76038c71feaa68fe9f88dabc7
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-01-06 15:44:03 +0800

    finished app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Learn about Hooks",
+      isCompleted: false
+    },
+    {
+      text: "Learn about Life",
+      isCompleted: false
+    }
+  ]);
+
+  // addTodo function takes in "text"
+  const addTodo = text => {
+    // create a newTodo array which contains original state and then the "text"
+    const newTodos = [...todos, { text }];
+    // set new todos state to newTodos
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+// addTodo function is passed down from App.js
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  // handleSubmit of form takes in an event i.e. the submission of the form
+  const handleSubmit = e => {
+    // prevent submission of form
+    e.preventDefault();
+    // if no value, then just return the form
+    if (!value) return;
+    // else addTodo function takes in the value we type in and adds it to newTodos
+    addTodo(value);
+    // set value of input to ""
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        // automatic value of input is an empty string "" i.e. the value of the state or whatever the value changes to when we type it in
+        value={value}
+        placeholder="Add Todo..."
+        // when the input is changed, we want to run a function and set the new state of the input to whatever we type in. without the following, whatever we type won't enter & it'll be a read-only field
+        onChange={e => setValue(e.target.value)}
+      ></input>
+    </form>
+  );
+};
+
+export default TodoForm;
