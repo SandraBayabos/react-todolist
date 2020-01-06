dfb30b4fa13311f7413e819bdee9f8e295f75b72
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-01-06 15:56:09 +0800

    refactored code

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 
 // addTodo function is passed down from App.js
 const TodoForm = ({ addTodo }) => {
-  const [value, setValue] = useState("");
+  const [text, setText] = useState("");
 
   // handleSubmit of form takes in an event i.e. the submission of the form
   const handleSubmit = e => {
     // prevent submission of form
     e.preventDefault();
     // if no value, then just return the form
-    if (!value) return;
+    if (!text) return;
     // else addTodo function takes in the value we type in and adds it to newTodos
-    addTodo(value);
+    addTodo(text);
     // set value of input to ""
-    setValue("");
+    setText("");
+  };
+
+  // enterItem takes in an event, which is the onChange event and sets the text to the value of whatever we have typed in
+  const enterItem = e => {
+    setText(e.target.value);
   };
 
   return (
@@ -22,10 +27,10 @@ const TodoForm = ({ addTodo }) => {
         type="text"
         className="input"
         // automatic value of input is an empty string "" i.e. the value of the state or whatever the value changes to when we type it in
-        value={value}
+        value={text}
         placeholder="Add Todo..."
         // when the input is changed, we want to run a function and set the new state of the input to whatever we type in. without the following, whatever we type won't enter & it'll be a read-only field
-        onChange={e => setValue(e.target.value)}
+        onChange={enterItem}
       ></input>
     </form>
   );
